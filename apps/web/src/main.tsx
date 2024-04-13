f7b20ee4992c75e1f7737fb2dc219cb5c4a3dbf6
Rodrigo Escobar Lopez <r.escobarl@uniandes.edu.co>
2024-04-13 15:35:58 -0500

    Feature: PSA-169 update styles in web app (#35)

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,9 +4,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '../config/lang/form'
 import '../config/lang/i18n'
+import { StyledEngineProvider } from '@mui/material'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<Router />
+		<StyledEngineProvider injectFirst>
+			<Router />
+		</StyledEngineProvider>
 	</React.StrictMode>
 )
